e23f08c61f932716a54427c3a7f62e850b87d0ea
Dmitri Kovalenko <dmitrikovalenko@MacBook-Pro-Dmitri.local>
2021-10-02 22:18:24 +0300

    Finally

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Generic; //для списку
 namespace democonsole
 {    
-   public class Pair<T, U> {
+   public class Pair<T, U> {    //Створив клас Пари, для зручності далі
     public Pair() {
     }
 
@@ -14,37 +14,37 @@ namespace democonsole
     public T First { get; set; }
     public U Second { get; set; }
 };
-    abstract class Student
+    abstract class Student //абстрактний клас
     {
-        public Student(string name){
-            _name = name;
+        public Student(string name){ //конструктор встановлює ім'я студента та
+            _name = name;           //порожній стейт
             _state = "";
         }
-        public void Relax(){
+        public void Relax(){ //метод додає до стейту відпочинок
             _state += "Relax ";
         }
-        public void Read(){
+        public void Read(){ //додає до стейту читання
             _state += "Read ";
         }
-        public void Write(){
+        public void Write(){ //додає до стейту писання
             _state += "Write ";
         }
-        public abstract void Study();
-        public string _name, _state;
-        public Pair<string, string> GetStudentInfo(){
+        public abstract void Study(); //абстрактний метод Study
+        public string _name, _state; //ім'я та стейт
+        public Pair<string, string> GetStudentInfo(){ //пара інформації про студента ім'я/стейт
             var pair = new Pair<string, string>(_name, _state);
             return pair;
         }
     }
     
     
-    class GoodStudent : Student
+    class GoodStudent : Student //клас, що успадковується від абстрактного класу
     {
-        public GoodStudent(string name)
-            :base(name){
+        public GoodStudent(string name) //конструктор цього класу викликає базовий конструктор 
+            :base(name){                //абстрактного класу та змінює значення стейт
             _state += "Good ";
         }
-        override public void Study(){
+        override public void Study(){ //перепризначаємо абстрактний метод
             Read();
             Write();
             Read();
@@ -52,7 +52,7 @@ namespace democonsole
             Relax();
         }
     }
-    class BadStudent : Student
+    class BadStudent : Student //все те ж саме, що і в GoodStudent
     {
         public BadStudent(string name)
             :base(name){
@@ -67,22 +67,22 @@ namespace democonsole
         }
     }
     
-    class Group
+    class Group //клас групи
     {
-        private string _name;
-        List<Student> list = new List<Student>();
-        public Group(string name){
+        private string _name; //назва групи
+        List<Student> list = new List<Student>(); //список студентів групи
+        public Group(string name){ //конструктор
             _name = name;
         }
-        public void AddStudent(Student st){
+        public void AddStudent(Student st){ //метод, що додає студента в групу
             list.Add(st);
         }
-        public void AddGroup(Group gr){
-            foreach (Student st in gr.list){
+        public void AddGroup(Group gr){     //метод, що додає студентів 
+            foreach (Student st in gr.list){//іншої групи в цю групу
                 AddStudent (st);
             }
         }
-        private string GetInfo(){
+        private string GetInfo(){ //генерує рядок з короткою інформацією про групу
             string info = "";
             info += _name +": ";
             foreach (var student in list){
@@ -90,7 +90,7 @@ namespace democonsole
             }
             return info;
         }
-        private string GetFullInfo(){
+        private string GetFullInfo(){ //генерує рядок з повною інформацією
             string info = "";
             info += _name +": ";
             foreach (var student in list){
@@ -98,10 +98,10 @@ namespace democonsole
             }
             return info;
         }
-        public void PrintInfo(){
+        public void PrintInfo(){ //метод, що виводить коротку інформацію
             Console.WriteLine(this.GetInfo());
         }
-        public void PrintFullInfo(){
+        public void PrintFullInfo(){ //метод, що виводить повну інформацію
             Console.WriteLine(this.GetFullInfo());
         }
     }
@@ -109,22 +109,22 @@ namespace democonsole
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //перевірка. Створуюємо 4 студента
             GoodStudent a = new GoodStudent("a");
             BadStudent b = new BadStudent("b");
             GoodStudent aa = new GoodStudent("aa");
             BadStudent bb = new BadStudent("bb");
-            Group K17 = new Group("K17");
+            Group K17 = new Group("K17"); //створюємо дві групи
             Group K18 = new Group("K18");
-            K17.AddStudent(a);
+            K17.AddStudent(a); //Додаємо студентів в дві групи
             K17.AddStudent(aa);
             K18.AddStudent(b);
             K18.AddStudent(bb);
-            K17.AddGroup(K18);
-            a.Study();
+            K17.AddGroup(K18); //додаємо студентів групи К18 до групи К17
+            a.Study(); //Студенти вчаться та змінюють значення стейтів
             b.Study();
-            K17.PrintFullInfo();
-            K17.PrintInfo();
+            K17.PrintFullInfo(); //виводимо повну інформацію про групу
+            K17.PrintInfo(); //виводимо трішки інформації)
         }    
         
     }
